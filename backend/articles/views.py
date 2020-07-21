227d9c8c84b85e119ff717a30ba6edba05172a32
강민석 <kim12503@naver.com>
2020-07-21 16:47:58 +0900

    .S03P12B307-34 | 완료 | 백엔드 기본세팅

---
+++ backend/articles/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import get_object_or_404
+
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+
+# from .models import Article, Comment
+# from .serializers import ArticleSerializer, ArticleListSerializer, CommentSerializer
+
+# Create your views here.

d47aedc822071b66261ff6a1e6a9351d52faaf1c
강민석 <kim12503@naver.com>
2020-07-22 21:33:01 +0900

    .S03P12B307-76 | 완료 | 이메일 속성 삭제

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -4,6 +4,5 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     userImage = models.ImageField()
-    userEmail = models.CharField(max_length=100)
     userIntro = models.TextField()
 

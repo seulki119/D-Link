97775d4463bf767372f0cae3ca97e8aa046679c1
강민석 <kim12503@naver.com>
2020-07-23 13:29:24 +0900

    .S03P12B307-80 | 완료 | 비밀번호 변경 API 추가

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -9,3 +9,12 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username',)
+
+class ChangePasswordSerializer(serializers.Serializer):
+    model = User
+
+    """
+    Serializer for password change endpoint.
+    """
+    old_password = serializers.CharField(required=True)
+    new_password = serializers.CharField(required=True)
--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from . import views
+# from django.views.decorators.csrf import csrf_exempt
+from .views import ChangePasswordView
 
 urlpatterns = [
-
+    path('password/', ChangePasswordView.as_view(), name='change-password'),
 ]

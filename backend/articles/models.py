1f10892f307b6e072989d6c49bb50244dd0fb7c3
Jin-N <cjswo291879@gmail.com>
2020-07-23 14:49:25 +0900

    .S03P12B307-76 | 진행중 | accounts모델수정

--- backend/articles/models.py
+++ backend/articles/models.py
@@ -5,32 +5,32 @@ from django.conf import settings
 # Create your models here.
 class Article(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    articleImage = models.ImageField(upload_to='photos')
-    articleHashTag = CharField(max_length=100)
-    articleCreatedAt = models.DateTimeField(auto_now_add=True)
-    articleUpdatedAt = models.DateTimeField(auto_now=True)
-    articleContent = models.TextField()
+    image = models.ImageField(upload_to='photos')
+    hashTag = models.CharField(max_length=100)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
+    content = models.TextField()
 
-    scrap = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name=scrap_set)
+    scrap = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='scrap_set')
 
 
 class Comment(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
-    commentContent = models.TextField()
-    commentCreatedAt = models.DateTimeField(auto_now_add=True)
-    commentUpdatedAt = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
     
-    like = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name=commentLike_set)
+    like = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='commentLike_set')
 
 class Recomment(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
-    recommentContent = models.TextField()
-    recommentCreatedAt = models.DateTimeField(auto_now_add=True)
-    recommentUpdatedAt = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
 
-    like = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name=recommentLike_set)
+    like = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='recommentLike_set')
 
 
 

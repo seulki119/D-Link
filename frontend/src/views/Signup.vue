33a882067d9890305cd5a65c43732afc8769b296
Seulki Park <seul6854ki@gmail.com>
2020-07-24 11:41:02 +0900

    .S03P12B307-56 | 수정 | nav 삭제
    App.vue에 nav를 삽입하였으므로 해당페이지에서는 nav삭제

--- frontend/src/views/Signup.vue
+++ frontend/src/views/Signup.vue
@@ -1,78 +1,64 @@
 <template>
-  <v-app id="inspire">
-    <v-app-bar app color="#F8C471" dark>
-      <v-spacer></v-spacer>
-    </v-app-bar>
+  <v-container class="fill-height" fluid>
+    <v-row align="center" justify="center">
+      <ValidationObserver ref="observer">
+        <v-form v-model="valid">
+          <ValidationProvider v-slot="{ errors }" name="email" rules="required|email">
+            <v-text-field v-model="email" :error-messages="errors" label="이메일" required></v-text-field>
+          </ValidationProvider>
 
-    <v-main>
-      <v-container class="fill-height" fluid>
-        <v-row align="center" justify="center">
-          <ValidationObserver ref="observer">
-            <v-form v-model="valid">
-              <ValidationProvider v-slot="{ errors }" name="email" rules="required|email">
-                <v-text-field v-model="email" :error-messages="errors" label="이메일" required></v-text-field>
-              </ValidationProvider>
+          <ValidationProvider name="password" rules="required|min:8|max:16" v-slot="{ errors }">
+            <v-text-field
+              v-model="password"
+              :counter="16"
+              type="password"
+              :error-messages="errors"
+              label="비밀번호"
+              required
+            ></v-text-field>
+          </ValidationProvider>
 
-              <ValidationProvider name="password" rules="required|min:8|max:16" v-slot="{ errors }">
-                <v-text-field
-                  v-model="password"
-                  :counter="16"
-                  type="password"
-                  :error-messages="errors"
-                  label="비밀번호"
-                  required
-                ></v-text-field>
-              </ValidationProvider>
+          <ValidationProvider
+            name="confirm"
+            rules="required|passswordConfirm:@password"
+            v-slot="{ errors }"
+          >
+            <v-text-field
+              v-model="confirmation"
+              :counter="password.length"
+              type="password"
+              :error-messages="errors"
+              label="비밀번호 확인"
+              required
+            ></v-text-field>
+          </ValidationProvider>
 
-              <ValidationProvider
-                name="confirm"
-                rules="required|passswordConfirm:@password"
-                v-slot="{ errors }"
-              >
-                <v-text-field
-                  v-model="confirmation"
-                  :counter="password.length"
-                  type="password"
-                  :error-messages="errors"
-                  label="비밀번호 확인"
-                  required
-                ></v-text-field>
-              </ValidationProvider>
+          <ValidationProvider v-slot="{ errors }" name="Name" rules="required|max:10">
+            <v-text-field
+              v-model="name"
+              :counter="10"
+              :error-messages="errors"
+              label="닉네임"
+              required
+            ></v-text-field>
+          </ValidationProvider>
 
-              <ValidationProvider v-slot="{ errors }" name="Name" rules="required|max:10">
-                <v-text-field
-                  v-model="name"
-                  :counter="10"
-                  :error-messages="errors"
-                  label="닉네임"
-                  required
-                ></v-text-field>
-              </ValidationProvider>
+          <ValidationProvider v-slot="{ errors }" rules="agree" name="checkbox">
+            <v-checkbox
+              v-model="checkbox"
+              :error-messages="errors"
+              value="1"
+              label="약관"
+              type="checkbox"
+              required
+            ></v-checkbox>
+          </ValidationProvider>
 
-              <ValidationProvider v-slot="{ errors }" rules="agree" name="checkbox">
-                <v-checkbox
-                  v-model="checkbox"
-                  :error-messages="errors"
-                  value="1"
-                  label="약관"
-                  type="checkbox"
-                  required
-                ></v-checkbox>
-              </ValidationProvider>
-
-              <v-btn :disabled="!valid" class="mr-4" @click="submit">다음</v-btn>
-            </v-form>
-          </ValidationObserver>
-        </v-row>
-      </v-container>
-    </v-main>
-
-    <v-footer color="#F8C471" app>
-      <v-spacer></v-spacer>
-
-      <span class="white--text">&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
-  </v-app>
+          <v-btn color="primary" :disabled="!valid" class="mr-4" @click="submit">다음</v-btn>
+        </v-form>
+      </ValidationObserver>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -135,8 +121,11 @@ export default {
   }),
 
   methods: {
-    submit() {
-      this.$refs.observer.validate();
+    async submit() {
+      const isValid = await this.$refs.observer.validate();
+      if (isValid) {
+        this.$router.push("taste");
+      }
     }
     // clear() {
     //   this.name = "";

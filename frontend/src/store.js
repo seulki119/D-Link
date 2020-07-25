805e76ff510374b55aef7b29ba5128025c62340e
Seulki Park <seul6854ki@gmail.com>
2020-07-25 13:45:58 +0900

    store.js에 axios 방식을 http로 변경

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,14 +2,15 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import router from "./router";
-import axios from "axios";
+import http from "@/util/http-common";
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
     userInfo: null,
     isLogin: false,
-    isLoginError: false
+    isLoginError: false,
   },
   mutations: {
     // 로그인 성공
@@ -29,28 +30,29 @@ export default new Vuex.Store({
       state.isLoginError = false;
       state.userInfo = null;
       localStorage.removeItem("token");
-    }
+    },
   },
   actions: {
     //로그인 시도
     login({ dispatch }, loginObj) {
       //로그인 -> 토큰 반환
-      axios.post("http://127.0.0.1:8000/rest-auth/login/", loginObj)
-        .then(res => {
+      http
+        .post("/rest-auth/login/", loginObj)
+        .then((res) => {
           // 로그인 성공시, token을 헤더에 포함시킴
           let token = res.data.key;
 
           // 토큰을 로컬스토리지에 저장
           localStorage.setItem("token", token);
-          this.dispatch("getUserInfo")
+          this.dispatch("getUserInfo");
         })
         .catch(() => {
-          alert('이메일과 비밀번호를 확인하세요.')
-        })
+          alert("이메일과 비밀번호를 확인하세요.");
+        });
     },
     logout({ commit }) {
-      commit("logout")
-      router.push({ name: "home" }).catch(error => {
+      commit("logout");
+      router.push({ name: "home" }).catch((error) => {
         if (error.name != "NavigationDuplicated") {
           throw error;
         }
@@ -62,26 +64,26 @@ export default new Vuex.Store({
 
       let config = {
         headers: {
-          "Authorization": `Token ${token}`
-        }
-      }
+          Authorization: `Token ${token}`,
+        },
+      };
       // 헤더 with 토큰 -> 유저 정보를 반환
       // 새로고침 하더라도 토큰만으로 계속 유저 정보를 요청하게 한다!
-      axios.get("http://127.0.0.1:8000/rest-auth/user/", config)
-        .then(response => {
+      http
+        .get("/rest-auth/user/", config)
+        .then((response) => {
           let userInfo = {
             pk: response.data.pk,
             username: response.data.username,
             email: response.data.email,
             first_name: response.data.first_name,
-            last_name: response.data.last_name
-          }
-          commit('loginSuccess', userInfo)
+            last_name: response.data.last_name,
+          };
+          commit("loginSuccess", userInfo);
         })
         .catch(() => {
-          alert('이메일과 비밀번호를 확인하세요.')
-        })
+          alert("이메일과 비밀번호를 확인하세요.");
+        });
     },
   },
-
 });

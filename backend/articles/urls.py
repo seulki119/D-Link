b17d176c9feb21c7213ffce38aef47825ecf2f68
강민석 <kim12503@naver.com>
2020-07-27 15:55:55 +0900

    .S03P12B307-91 | 진행중 | 스크랩 등록 API 추가

--- backend/articles/urls.py
+++ backend/articles/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('create/', views.create, name='create'),
     path('<int:article_pk>/', views.detail, name='detail'),
     path('<int:article_pk>/ud/', views.update_delete, name='update_delete'),
+    path('<int:article_pk>/scrap/', views.scrap, name='scrap'),
 ]

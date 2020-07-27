28b9b64de805548c984dade396b107bf80cd4c75
hwandong.joo <hwandong.joo@gmail.com>
2020-07-27 17:09:13 +0900

    .S03P12B307-46 | 추가| 이미지 업로드 구현
    업로드 버튼 및 이미지 미리보기 구현 완료

--- frontend/src/views/CreateArticle.vue
+++ frontend/src/views/CreateArticle.vue
@@ -1,8 +1,74 @@
-<template></template>
+<template>
+  <v-container class="fill-height" style="max-width:450px">
+    <div id="app">
+      <v-app id="inspire">
+        <v-card class="mx-auto" max-width="400" v-if="file">
+          <v-img :src="preview" class="img-fluid" />
+        </v-card>
+        <v-spacer></v-spacer>
+        <v-file-input
+          small-chips
+          color="deep-purple accent-4"
+          placeholder="업로드할 파일을 골라주세요"
+          accept="image/*"
+          label="사진 업로드"
+          prepend-icon="mdi-camera"
+          outlined
+          dense
+          :show-size="1000"
+          v-model="file"
+          @change="add"
+        ></v-file-input>
+      </v-app>
+    </div>
+  </v-container>
+</template>
 
 <script>
-export default {};
+export default {
+  //   data: () => ({
+  //     file: [],
+  //     reader: []
+  //   }),
+
+  //   methods: {
+  //     add() {
+  //       console.log("file", this.file);
+  //       this.file.forEach((fi, f) => {
+  //         this.reader[f] = new FileReader();
+  //         this.reader[f].onloadend = e => {
+  //           let fileData = this.reader[f].result;
+  //           let imgRef = this.$refs.fi[f];
+  //           imgRef.src = fileData;
+  //           console.log(fileData);
+  //           console.log(e);
+  //           // send to server here...
+  //         };
+
+  //         this.reader[f].readAsDataURL(this.file[f]);
+  //       });
+  //     }
+  //   }
+  data() {
+    return {
+      file: null,
+      reader: null,
+      preview: null
+    };
+  },
+  methods: {
+    add() {
+      this.reader = new FileReader();
+      this.reader.onloadend = () => {
+        let fileData = this.reader.result;
+        this.preview = fileData;
+        // send to server here...
+      };
+      this.reader.readAsDataURL(this.file);
+    }
+  }
+};
 </script>
 
-<style>
+<style scoped lang="scss">
 </style>

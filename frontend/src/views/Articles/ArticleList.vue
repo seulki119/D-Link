bb2275baf782a7eea3f4b7f13a6ba3cfddacc765
Seulki Park <seul6854ki@gmail.com>
2020-07-27 16:20:40 +0900

    .S03P12B307-51 | 수정 | 백앤드 통신 추가
    데모이미지 띄우던 형태에서, 백앤드 통신하여 실제 데이터 띄우도록 수정함.
    백앤드 경로에서 이미지 가져오는 방법 수정 필요

--- frontend/src/views/Articles/ArticleList.vue
+++ frontend/src/views/Articles/ArticleList.vue
@@ -2,45 +2,69 @@
   <div class="container">
     <StackGrid :columnWidth="200" :gutterX="20" :gutterY="20">
       <!-- you component like this -->
-      <div class="stack-item" v-for="(item, index) in 9" :key="index">
+      <div class="stack-item" v-for="(item, index) in items" :key="index">
         <!-- some thing have fixed height-->
         <img
-          :src="`https://picsum.photos/500/300?image=${index * 5 + 10}`"
+          :src="item.image"
           alt=""
-          @click="showDetail(item.no)"
+          @click="showDetail(item.id)"
           style="cursor: pointer;"
         />
-        <img style="right:100%" src="@/assets/turned_in_not-24px.svg" alt="" />
-        {{ item.scrap }}
+        <img
+          @click="scrapAction(item.id)"
+          style="right:100%"
+          src="@/assets/turned_in_not-24px.svg"
+          alt=""
+        />
+        {{ item.scrap[0] }}
       </div>
     </StackGrid>
   </div>
 </template>
 <script>
 import StackGrid from "vue-stack-grid-component";
-// import http from "@/util/http-common";
+import http from "@/util/http-common";
 
 export default {
   components: {
     StackGrid,
   },
-  // data: {
-  //   items: [], //article에는 고유번호(article.id)/이미지src/스크랩횟수
-  // },
-  // created() {
-  //   http
-  //     .get("/articles")
-  //     .then((response) => {
-  //       this.items = response.items;
-  //     })
-  //     .catch((error) => {
-  //       console.dir(error.data);
-  //     });
-  // },
+  data() {
+    return {
+      items: [
+        {
+          id: "",
+          user: {
+            id: "",
+            username: "",
+            image: "",
+          },
+          image: "",
+          hashTag: "",
+          content: "",
+          scrap: [],
+        },
+      ],
+    }; //article에는 고유번호(article.id)/이미지src/스크랩횟수
+  },
+  created() {
+    http
+      .get("/articles")
+      .then((response) => {
+        console.log(response);
+        this.items = response.data;
+      })
+      .catch((error) => {
+        console.dir(error.data);
+      });
+  },
   methods: {
-    showDetail(no) {
-      this.$router.push(`article/${no}`);
+    showDetail(id) {
+      this.$router.push(`article?id=${id}`);
     },
+    // scrapAction(id) {
+    //   // 스크랩 처리
+    // },
   },
 };
 </script>

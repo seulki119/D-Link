49241ba3d42977f307e702955499ca36373a6852
강민석 <kim12503@naver.com>
2020-07-28 15:14:36 +0900

    .S03P12B307-91 | 완료 | 피드 스크랩 API정리
    댓글 대댓글 시리얼라이저 추가

--- backend/articles/serializers.py
+++ backend/articles/serializers.py
@@ -1,8 +1,9 @@
 from rest_framework import serializers
-from .models import Article, Comment
+from .models import Article, Comment, Recomment
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
+
 
 class UserSerializer(serializers.ModelSerializer):
 
@@ -10,13 +11,29 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ('id', 'username', 'image')
 
+class RecommentSerializer(serializers.ModelSerializer):
+    user = UserSerializer(required=False)
+
+    class Meta:
+        model = Recomment
+        fields = ('id', 'user', 'content', 'createdAt', 'updatedAt')
+
+class CommentSerializer(serializers.ModelSerializer):
+    recommentSet = RecommentSerializer(many=True, read_only=True, source='recomment_set')
+    user = UserSerializer(required=False)
+
+    class Meta:
+        model = Comment
+        fields = '__all__'
+
 class ArticleSerializer(serializers.ModelSerializer):
     user = UserSerializer(required=False)
+    commentSet = CommentSerializer(many=True, read_only=True, source='comment_set')
 
     class Meta:
         model = Article
         fields = '__all__'
-        read_only_fields = ('id', 'user', 'created_at', 'updated_at')
+        read_only_fields = ('id', 'user', 'createdAt', 'updatedAt')
 
 class ArticleCreateSerializer(serializers.ModelSerializer):
     user = UserSerializer(required=False)

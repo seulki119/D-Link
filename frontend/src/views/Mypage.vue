c0028c73f5d31aaec82a788879a587bea8f5a826
hwandong.joo <hwandong.joo@gmail.com>
2020-07-28 18:41:01 +0900

    .S03P12B307-30 | 시작 | 마이페이지
    MyPage 시작

--- frontend/src/views/Mypage.vue
+++ frontend/src/views/Mypage.vue
@@ -1,7 +1,18 @@
 <!-- eslint-disable -->
+<!--
+1. 회원 아이디 기반으로 정보 가져오기
+2. 회원이 스크랩한 게시물 목록
+3. 회원이 업로드한 게시물 목록
+4. 회원정보 관련 버튼(disabled 처리)
+      - 수정
+        -탈퇴
+      - 로그아웃
+
+-->
 <template>
   <div>
     <h1>{{userInfo.username}}님 환영합니다.</h1>
+    <h1>{{userInfo.username}}님 왜그러세요 ㅠㅠ</h1>
   </div>
 </template>
 

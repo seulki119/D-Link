1dc6a528b20af1a471e32c90c25aaea9eccedd99
Seulki Park <seul6854ki@gmail.com>
2020-07-28 13:32:43 +0900

    .S03P12B307-50 | 수정 | 피드상세 구성 변경

--- frontend/src/views/Articles/Article.vue
+++ frontend/src/views/Articles/Article.vue
@@ -4,31 +4,23 @@
       <v-list-item>
         <v-list-item-avatar color="grey"></v-list-item-avatar>
         <v-list-item-content>
-          <v-list-item-title class="headline">
-            {{ user.username }}
-          </v-list-item-title>
+          <v-list-item-title class="headline">{{ user.username }}</v-list-item-title>
         </v-list-item-content>
       </v-list-item>
       <v-img :src="`${image}`" height="194"></v-img>
 
       <v-card-text>
-        {{ content }}
+        <div style="float:right;">
+          {{ scrap.length }}
+          <img style="cursor: pointer;" src="@/assets/turned_in_not-24px.svg" alt />
+        </div>
       </v-card-text>
-      <v-card-text>
-        {{ hashTag }}
-      </v-card-text>
+      <v-card-text>{{ content }}</v-card-text>더보기
+      <v-card-text>{{ hashTag }}</v-card-text>
 
       <v-card-actions>
-        <v-btn text color="deep-purple accent-4">
-          댓글
-        </v-btn>
+        <v-btn text color="deep-purple accent-4">댓글 모두보기</v-btn>
         <v-spacer></v-spacer>
-        <img
-          style="right:100%;cursor: pointer;"
-          src="@/assets/turned_in_not-24px.svg"
-          alt=""
-        />
-        {{ scrap.length }}
       </v-card-actions>
     </v-card>
   </div>
@@ -44,12 +36,12 @@ export default {
       user: {
         id: "",
         username: "",
-        image: "",
+        image: ""
       },
       image: "",
       hashTag: "",
       content: "",
-      scrap: [],
+      scrap: []
       // replies: {},
     };
   },
@@ -57,7 +49,7 @@ export default {
     // this.id = this.$route.query.id;
     // console.log(this.id);
     // 백엔드에서 해당id로 게시물정보 가져오기.
-    http.get(`/articles/${this.$route.query.id}`).then((response) => {
+    http.get(`/articles/${this.$route.query.id}`).then(response => {
       console.log(response);
       this.id = response.data.id;
       this.user.id = response.data.user.id;
@@ -69,6 +61,6 @@ export default {
       this.scrap = response.data.scrap;
       console.log(this.image);
     });
-  },
+  }
 };
 </script>

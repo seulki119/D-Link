73072170c2ef07fb984314577f9b828c4fa96841
hwandong.joo <hwandong.joo@gmail.com>
2020-07-28 13:50:09 +0900

    .S03P12B307-46 | 수정 | 클래스명 변경
    클래스명 변경 및 버튼 위치 조정

--- frontend/src/views/CreateArticle.vue
+++ frontend/src/views/CreateArticle.vue
@@ -2,44 +2,38 @@
   <v-container class="fill-height" style="max-width:450px">
     <div id="app">
       <v-app id="inspire">
-        <v-card class="mx-auto" max-width="400" v-if="file">
-          <v-img :src="preview" class="img-fluid" />
-          <v-divider></v-divider>
-          <v-combobox
-            v-model="select"
-            :items="items"
-            :color="red"
-            label="I use a scoped slot"
-            multiple
-            chips
+        <v-card class="px-5" max-width="400">
+          <v-img :src="preview" class="img-fluid ma-5" />
+          <v-file-input
+            small-chips
+            color="deep-purple accent-4"
+            placeholder="업로드할 파일을 골라주세요"
+            accept="image/*"
+            label="사진 업로드"
+            prepend-icon="mdi-camera"
+            outlined
             dense
-          >
-            <template v-slot:no-data>
+            :show-size="1000"
+            v-model="file"
+            @change="add"
+            class="pt-6 mx-6"
+          ></v-file-input>
+          <v-divider></v-divider>
+          <v-combobox v-model="tag" :items="items" label="태그 입력하세요" multiple chips dense>
+            <!-- <template v-slot:no-data>
               <v-list-item>
                 <span class="subheading">Create</span>
                 <v-chip :color="red" label small>{{ search }}</v-chip>
               </v-list-item>
-            </template>
+            </template>-->
           </v-combobox>
           <v-divider></v-divider>
           <v-card-text class="text--primary">
             <v-textarea v-model="content" label="내용" counter maxlength="120" full-width single-line></v-textarea>
           </v-card-text>
+
+          <v-divider></v-divider>
         </v-card>
-        <v-divider></v-divider>
-        <v-file-input
-          small-chips
-          color="deep-purple accent-4"
-          placeholder="업로드할 파일을 골라주세요"
-          accept="image/*"
-          label="사진 업로드"
-          prepend-icon="mdi-camera"
-          outlined
-          dense
-          :show-size="1000"
-          v-model="file"
-          @change="add"
-        ></v-file-input>
       </v-app>
     </div>
   </v-container>
@@ -52,7 +46,7 @@ export default {
       file: null,
       reader: null,
       preview: null,
-      select: [],
+      tag: [],
       items: ["맥주", "소주"],
       content: null
     };
@@ -66,6 +60,14 @@ export default {
         // send to server here...
       };
       this.reader.readAsDataURL(this.file);
+    }
+  },
+  watch: {
+    tag: function() {
+      console.log(this.tag);
+    },
+    content: function() {
+      console.log(this.content);
     }
   }
 };

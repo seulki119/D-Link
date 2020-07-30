87d7a99cad9b2f2e68bab90c7ca239fd256167ad
hwandong.joo <hwandong.joo@gmail.com>
2020-07-30 16:28:03 +0900

    .S03P12B307-74 | 수정 | 헤더 푸터 변경2
    아이콘 등 변경중

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,48 +2,65 @@
 <template>
   <v-app id="inspire">
     <v-app-bar
-      v-if="isLogin"
       app
-      grow
+      v-if="isLogin"
       color="indigo"
       dark
       max-width="600"
       min-width="300"
+      elevate-on-scroll
       class="mx-auto"
     >
-      <!--    im so crazy -->
-      <v-btn v-if="canBack" icon router @click="router.go(-1)">
-        <v-icon left>mdi-history</v-icon>뒤로가기
+      <v-btn class="mx-auto" v-if="canBack" icon @click="$router.go(-1)">
+        <!-- <div class="d-flex flex-column align-center"> -->
+        <v-icon>mdi-history</v-icon>
+        <!-- <p>뒤로가기</p> -->
+        <!-- </div> -->
       </v-btn>
-      <v-spacer></v-spacer>
-      <v-btn icon @click="$store.dispatch('logout')">
-        <v-icon left>mdi-heart</v-icon>로그아웃
-      </v-btn>
-      <v-spacer></v-spacer>
-      <v-btn icon router :to="{ name: 'mypage' }">
-        <v-icon left>mdi-map-marker</v-icon>마이페이지
+      <div class="d-flex flex-column align-center">
+        <v-icon>일단 여기다가 로고나 넣자 !</v-icon>
+        <!-- <p>로고</p> -->
+      </div>
+      <v-btn class="mx-auto" icon router :to="{ name: 'mypage' }">
+        <!-- <div class="d-flex flex-column align-center"> -->
+        <v-icon>mdi-account</v-icon>
+        <!-- <p>마이페이지</p> -->
+        <!-- </div> -->
       </v-btn>
     </v-app-bar>
 
     <v-main>
       <router-view></router-view>
     </v-main>
-    <v-bottom-navigation v-if="isLogin" grow color="teal">
-      <v-btn icon router :to="{ name: 'articlelist' }">
-        Home
-        <v-icon>mdi-home</v-icon>
-      </v-btn>
+    <v-footer app>
+      <v-bottom-navigation
+        v-if="isLogin"
+        grow
+        color="teal"
+        dark
+        max-width="600"
+        min-width="300"
+        class="mx-auto"
+      >
+        <v-btn icon router :to="{ name: 'articlelist' }">
+          홈
+          <v-icon>mdi-home</v-icon>
+        </v-btn>
+        <v-btn icon router :to="{ name: 'articlelist' }">
+          검색
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+        <v-btn prepend-icon="mdi-plus" icon router :to="{ name: 'createArticle' }">
+          파일 업로드
+          <v-icon>mdi-plus</v-icon>
+        </v-btn>
 
-      <v-btn icon router :to="{ name: 'createArticle' }">
-        파일 업로드
-        <v-icon>mdi-history</v-icon>
-      </v-btn>
-
-      <v-btn>
-        마이페이지
-        <v-icon>mdi-account</v-icon>
-      </v-btn>
-    </v-bottom-navigation>
+        <v-btn icon router :to="{ name: 'mypage' }">
+          마이페이지
+          <v-icon>mdi-account</v-icon>
+        </v-btn>
+      </v-bottom-navigation>
+    </v-footer>
   </v-app>
 </template>
 
@@ -55,14 +72,22 @@ export default {
   },
   data: () => ({
     drawer: null,
-    canBack: false
+    canBack: true
   }),
   computed: {
     ...mapState(["isLogin"])
   },
-  methods: {
-    back() {
-      console.log(this.$router.options.routes);
+  beforeUpdate() {
+    this.canBack = true;
+    let currentUrl = this.$router.history.current.name;
+    let baseUrl = this.$router.options.routes;
+    console.log(this.$router.options.routes);
+    console.log(this.$router.history.current.name);
+    for (let i = 0; i < 6; i++) {
+      if (baseUrl[i].name === currentUrl) {
+        this.canBack = false;
+        return;
+      }
     }
   }
 };

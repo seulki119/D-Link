2af602d200437b79ad6b6e17de0ea3efee444297
hwandong.joo <hwandong.joo@gmail.com>
2020-07-30 10:40:54 +0900

    .S03P12B307-30 | 추가 | 탭 기능
    탭에 따라서 업로드/스크랩 게시물 구분하여 출력

--- frontend/src/views/Mypage.vue
+++ frontend/src/views/Mypage.vue
@@ -10,15 +10,15 @@
 
 -->
 <template>
-  <v-container style="max-width:600px">
-    <v-card class="pa-5" max-width="600" min-width="300">
-      <v-row class="pa-5" no-gutters align="end">
+  <v-container max-width="600" min-width="300">
+    <v-card class="pa-5" max-width="600">
+      <v-row class="pa-5" no-gutters align="end" dense>
         <v-col class="pa-0">
           <v-avatar class="profileImage" color="grey" size="80" round>
             <v-img src="image"></v-img>
           </v-avatar>
         </v-col>
-        <v-col align="center" cols="20">
+        <v-col align="center">
           <v-list-item>
             <v-list-item-content>
               <v-list-item-title>{{username}}</v-list-item-title>
@@ -28,21 +28,48 @@
         </v-col>
       </v-row>
       <v-divider></v-divider>
-      <v-row>
-        <v-col cols="12">
-          <v-card>
+
+      <!-- <v-col cols="12"> -->
+      <v-card>
+        <v-tabs centered icons-and-text background-color="white" color="deep-purple accent-4">
+          <v-tab>업로드한 게시물</v-tab>
+          <v-tab>스크랩한 게시물</v-tab>
+          <v-tab-item>
             <v-container fluid>
               <v-row>
-                <v-col v-for="n in articleSet" :key="n" class="d-flex child-flex" cols="4">
+                <v-col v-for="n in articleSet" :key="n.image" cols="4">
+                  <v-img
+                    :src="`//127.0.0.1:8000/${n.image}`"
+                    class="grey lighten-2 pa-1"
+                    aspect-ratio="1"
+                  ></v-img>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-tab-item>
+          <!-- <v-tab-item>
+            <v-container fluid>
+              <v-row>
+                <v-col
+                  v-for="n in scrapSet"
+                  :key="n.image"
+                  class="d-flex child-flex"
+                  cols="12"
+                  md="4"
+                >
                   <v-card flat tile class="d-flex">
-                    <v-img :src="`//127.0.0.1:8000/${n.image}`" class="grey lighten-2 pa-1"></v-img>
+                    <v-img
+                      :src="`//127.0.0.1:8000/${n.image}`"
+                      class="grey lighten-2 pa-1"
+                      aspect-ratio="1"
+                    ></v-img>
                   </v-card>
                 </v-col>
               </v-row>
             </v-container>
-          </v-card>
-        </v-col>
-      </v-row>
+          </v-tab-item>-->
+        </v-tabs>
+      </v-card>
     </v-card>
   </v-container>
 </template>

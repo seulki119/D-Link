288f83b2ba1ffd542ee18dba99c40a8779a19576
yongwon <asdfg0237@naver.com>
2020-08-06 14:04:03 +0900

    .S03P12B307-36 | 수정 | dist gitignore

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='index.html')),
     path('rest-auth/', include('rest_auth.urls')),
     path('rest-auth/signup/', include('rest_auth.registration.urls')),
     path('accounts/', include('accounts.urls')),

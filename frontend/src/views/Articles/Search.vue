605c176380c250f1a19751d0130e83ce201008e8
hwandong.joo <hwandong.joo@gmail.com>
2020-08-06 17:11:19 +0900

    .S03P13B307-49 | 완료 | 검색
    검색 기능 완료

--- frontend/src/views/Articles/Search.vue
+++ frontend/src/views/Articles/Search.vue
@@ -1,49 +1,76 @@
 <template>
-  <v-app-bar
-    app
-    color="indigo"
-    dark
-    max-width="600"
-    min-width="300"
-    elevate-on-scroll
-    class="mx-auto"
-  >
-    <v-icon class="mx-auto">mdi-magnify</v-icon>
-    <v-spacer></v-spacer>
-    <v-autocomplete
-      class="pt-2"
-      v-model="tag"
-      :items="tags"
-      :counter="3"
-      dense
-      chips
-      small-chips
-      multiple
-      hide-no-data
-      hide-selected
-      deletable-chips
-      no-data-text
-      hint="Maximum of 3 tags"
-    ></v-autocomplete>
-    <v-spacer></v-spacer>
-    <v-btn icon class="mx-auto" v-show="tag.length !==0 " @click="search()">검색</v-btn>
-  </v-app-bar>
+  <div>
+    <v-app-bar
+      app
+      color="indigo"
+      dark
+      max-width="600"
+      min-width="300"
+      elevate-on-scroll
+      class="mx-auto"
+    >
+      <v-icon class="mx-auto">mdi-magnify</v-icon>
+      <v-spacer></v-spacer>
+      <v-autocomplete
+        class="pt-2"
+        v-model="tag"
+        :items="tags"
+        :counter="3"
+        dense
+        chips
+        small-chips
+        multiple
+        hide-no-data
+        hide-selected
+        deletable-chips
+        no-data-text
+        hint="Maximum of 3 tags"
+      ></v-autocomplete>
+      <v-spacer></v-spacer>
+      <v-btn icon class="mx-auto" v-show="tag.length !==0 " @click="search()">검색</v-btn>
+    </v-app-bar>
+    <v-container max-width="600" min-width="300">
+      <v-card class="mx-auto pa-5" max-width="600">
+        <StackGrid :columnWidth="210" :gutterX="20" :gutterY="20">
+          <!-- you component like this -->
+          <div
+            class="stack-item"
+            v-for="(item, index) in searchList.slice().reverse()"
+            :key="index"
+          >
+            <!-- some thing have fixed height-->
+            <div v-if="item.user.id != userId" class="stack-item stack-item-6">
+              <img
+                :src="`//127.0.0.1:8000/${item.image}`"
+                alt
+                @click="showDetail(item.id)"
+                style="cursor: pointer;width:200px;"
+              />
+            </div>
+          </div>
+        </StackGrid>
+      </v-card>
+    </v-container>
+  </div>
 </template>
+
 <script>
-// import StackGrid from "vue-stack-grid-component";
+import StackGrid from "vue-stack-grid-component";
 import http from "@/util/http-common";
+import { mapGetters } from "vuex";
 export default {
-  // components: {
-  //   StackGrid
-  // },
+  components: {
+    StackGrid
+  },
   data() {
     return {
       tag: [],
       tags: [],
-      scrapSrc: "",
-      scrapNo: "https://img.icons8.com/carbon-copy/24/000000/wine-glass.png",
-      scrapYes: "https://img.icons8.com/plasticine/24/000000/wine-glass.png"
+      searchList: []
     };
+  },
+  computed: {
+    ...mapGetters(["userId"])
   },
   created() {
     let token = localStorage.getItem("token");
@@ -65,19 +92,7 @@ export default {
     showDetail(id) {
       this.$router.push(`article?id=${id}`);
     },
-    scrapAct(id) {
-      this.$store.dispatch("doScrap", {
-        url: `/articles/${id}/scrap`,
-        page: "articlelist"
-      });
-      // scrap 동작시 아이콘src 토글
 
-      // if (this.scrapSrc == this.scrapYes) {
-      //   this.scrapSrc = this.scrapNo;
-      // } else {
-      //   this.scrapSrc = this.scrapYes;
-      // }
-    },
     search() {
       let token = localStorage.getItem("token");
 
@@ -99,6 +114,7 @@ export default {
         .post("/articles/search/", fd, config)
         .then(res => {
           console.log(res);
+          this.searchList = res.data;
         })
         .catch(err => {
           console.log(err.response);
@@ -115,11 +131,4 @@ export default {
 };
 </script>
 <style>
-.scrapInfo {
-  right: 100%;
-  float: right;
-}
-.scrapInfo img {
-  cursor: pointer;
-}
 </style>

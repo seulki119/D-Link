8601236fe4ae9cc532175424fbd9ab3bd96a9eb3
강민석 <kim12503@naver.com>
2020-08-11 14:54:42 +0900

    S03P13B307-1 | 진행중 | 알림 API 추가

--- frontend/src/util/http-common.js
+++ frontend/src/util/http-common.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 // axios 객체 생성
 export default axios.create({
-  baseURL: "http://i3b307.p.ssafy.io/api",
+  baseURL: "http://127.0.0.1:8000/api",
+  // http://i3b307.p.ssafy.io/
+  // http://127.0.0.1:8000/
   headers: {
     "Content-type": "application/json",
   },

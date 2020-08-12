d814e96112c01e2d5276c25d676819c05f215c3d
강민석 <kim12503@naver.com>
2020-08-12 11:05:02 +0900

    S03P13B307-71 | 진행중 | https로 변경

--- frontend/src/util/http-common.js
+++ frontend/src/util/http-common.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 // axios 객체 생성
 export default axios.create({
-  baseURL: "http://i3b307.p.ssafy.io/api",
-  // http://i3b307.p.ssafy.io/api
-  // http://127.0.0.1:8000/api
+  baseURL: "https://i3b307.p.ssafy.io/api",
+  // https://i3b307.p.ssafy.io/api
+  // https://127.0.0.1:8000/api
   headers: {
     "Content-type": "application/json",
   },

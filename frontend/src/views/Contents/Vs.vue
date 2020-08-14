e58837e9894ae3f6072d80ecc8f722edde2bbea3
hwandong.joo <hwandong.joo@gmail.com>
2020-08-14 17:13:38 +0900

    .S03P13B307-78 |  추가 |  버튼 등
    투표 기능 추가 완료 -> 투표 결과 출력 추가 필요

--- frontend/src/views/Contents/Vs.vue
+++ frontend/src/views/Contents/Vs.vue
@@ -1,25 +1,41 @@
 <template>
   <v-container>
     <v-card class="mx-auto pa-5" max-width="600">
-      <v-toolbar dark flat>
-        <v-toolbar-title>당신의 선택은?</v-toolbar-title>
-      </v-toolbar>
-      <v-row class="justify-center mx-auto">
-        <v-radio-group v-model="choice">
+      <div v-if="!selected">
+        <v-toolbar dark flat>
+          <v-toolbar-title>당신의 선택은?</v-toolbar-title>
+        </v-toolbar>
+        <v-row class="justify-center mx-auto">
+          <v-radio-group v-model="choice" mandatory>
+            <v-layout row class="align-end">
+              <v-flex column>
+                <v-img :src="`//i3b307.p.ssafy.io/${image[0]}`" class="grey lighten-2 ma-3"></v-img>
+                <v-radio value="0"></v-radio>
+              </v-flex>
+              <v-flex column>
+                <v-img :src="`//i3b307.p.ssafy.io/${image[1]}`" class="grey lighten-2 ma-3"></v-img>
+                <v-radio value="1"></v-radio>
+              </v-flex>
+            </v-layout>
+          </v-radio-group>
+        </v-row>
+        <v-divider></v-divider>
+        <v-btn block color="indigo lighten-1 white--text" @click="vote()">{{topic[choice]}}에 투표하기</v-btn>
+      </div>
+      <div v-else>
+        <v-toolbar dark flat>
+          <v-toolbar-title>당신의 선택은 {{topic[last]}}였습니다.</v-toolbar-title>
+        </v-toolbar>
+        <v-row class="justify-center mx-auto">
           <v-layout row class="align-end">
             <v-flex column>
-              <v-img :src="`//i3b307.p.ssafy.io/${image_A}`" class="grey lighten-2 ma-3"></v-img>
-              <v-radio value="1" class="text-center"></v-radio>
-            </v-flex>
-            <v-flex column>
-              <v-img :src="`//i3b307.p.ssafy.io/${image_B}`" class="grey lighten-2 ma-3"></v-img>
-              <v-radio value="2"></v-radio>
+              <v-img :src="`//i3b307.p.ssafy.io/${image[last]}`" class="grey lighten-2 ma-3"></v-img>
             </v-flex>
           </v-layout>
-        </v-radio-group>
-      </v-row>
-      <v-divider></v-divider>
-      <v-row>dafasdfads</v-row>
+        </v-row>
+        <v-diver></v-diver>
+        <span>대충 결과 관련 내용 출력</span>
+      </div>
     </v-card>
   </v-container>
 </template>
@@ -29,14 +45,15 @@ import http from "@/util/http-common";
 export default {
   data() {
     return {
-      image_A: "",
-      image_B: "",
-      topic_A: "",
-      topic_B: "",
+      id: [],
+      image: [],
+      topic: [],
       createdAt: "",
       select_A: [],
       select_B: [],
-      selected: false
+      choice: "",
+      selected: false,
+      last: ""
     };
   },
   created() {
@@ -50,21 +67,21 @@ export default {
       .get("versus", config)
       .then(res => {
         let data = res.data[0];
+        this.id = data.id;
         this.createdAt = data.createdAt;
-        this.image_A = data.image_A;
-        this.image_B = data.image_B;
-        this.topic_A = data.topic_A;
-        this.topic_B = data.topic_B;
+        this.image.push(data.image_A);
+        this.image.push(data.image_B);
+        this.topic.push(data.topic_A);
+        this.topic.push(data.topic_B);
         console.log(this.image_A);
         let userId = this.$store.getters.userId;
-        if (data.select_A.includes(userId) || data.select_B.includes(userId)) {
+        if (data.select_A.includes(userId)) {
           this.selected = true;
-          console.log("이미 선택함");
-        } else {
-          console.log(userId);
-          console.log("아직 선택안함");
+          this.last = 0;
+        } else if (data.select_B.includes(userId)) {
+          this.selected = true;
+          this.last = 1;
         }
-        this.loading = true;
       })
       .catch(err => {
         console.log(err);
@@ -101,6 +118,24 @@ export default {
         .catch(err => {
           console.log(err);
         });
+    },
+    vote() {
+      let token = localStorage.getItem("token");
+      let config = {
+        headers: {
+          Authorization: `Token ${token}`
+        }
+      };
+      let data = new FormData();
+      data.append("select", this.choice === 1 ? "A" : "B");
+      http.post(`/versus/${this.id}/vote`, data, config).then(res => {
+        console.log(res);
+      });
+    }
+  },
+  watch: {
+    choice() {
+      console.log(this.choice);
     }
   }
 };

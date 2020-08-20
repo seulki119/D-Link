e8f42b56265467f741abdcb71f606d6a58bc4cbf
hwandong.joo <hwandong.joo@gmail.com>
2020-08-21 02:40:26 +0900

    .S03P13B307-120 | 추가 및 수정| 로고
    로고 추가 및 각 페이지 버튼 재설정

--- frontend/src/views/Contents/ContentList.vue
+++ frontend/src/views/Contents/ContentList.vue
@@ -3,28 +3,22 @@
     <v-container max-width="600" min-width="300">
       <v-row>
         <v-col cols="12">
-          <v-card color="#D4B3E3" dark>
+          <v-card color="#D4B3E3" dark router :to="{ name: 'cocktail' }">
             <div class="d-flex flex-no-wrap justify-space-between">
               <div>
                 <v-card-title class="text-h4 font-weight-bold">칵테일</v-card-title>
                 <v-card-subtitle class="text-h4">취향조사</v-card-subtitle>
               </div>
-              <!-- <v-avatar class="ma-3" size="125" tile>
-                <v-img :src="item.src"></v-img>
-              </v-avatar>-->
             </div>
           </v-card>
         </v-col>
         <v-col cols="12">
-          <v-card color="#EBA2A0" dark>
+          <v-card color="#EBA2A0" dark router :to="{ name: 'vslist' }">
             <div class="d-flex flex-no-wrap justify-space-between">
               <div>
                 <v-card-title class="text-h4 font-weight-bold">누가</v-card-title>
                 <v-card-subtitle class="text-h4">국룰이지?</v-card-subtitle>
               </div>
-              <!-- <v-avatar class="ma-3" size="125" tile>
-                <v-img :src="item.src"></v-img>
-              </v-avatar>-->
             </div>
           </v-card>
         </v-col>
